aa2914d111be2c5741800048fec28f8d38bd56b2
Mohamed Sabry Basha <m-sabry89@hotmail.com>
2020-09-12 22:59:28 +0300

    best version 2

--- app/src/main/java/android/example/searchmovies/utils/NetWortUtils.java
+++ app/src/main/java/android/example/searchmovies/utils/NetWortUtils.java
@@ -13,7 +13,7 @@ public class NetWortUtils {
 
     final static String URL = "http://api.themoviedb.org/3/movie/";
     final static String PARAM_API_KEY = "api_key";
-    final static String api_key = "70a7ac822e4e34666123cfe398a4bac8";
+    final static String api_key = "";
     final static String PARAM_LANGUAGE = "language";
     final static String language = "en-US";
     final static String REVIEW = "reviews";
